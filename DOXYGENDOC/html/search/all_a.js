31af198b74465405636bd568782b48cb73aeeacc
Ubuntu_16.04 <maximilian.gruendel@gmail.com>
2020-12-15 13:28:58 +0100

    Klassenübersicht

---
+++ DOXYGENDOC/html/search/all_a.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['minimal_5fpwd_5flength',['MINIMAL_PWD_LENGTH',['../de/dff/namespaceTASK1.html#acd3fa6a901e7ec468c3245ae531a295a',1,'TASK1']]],
+  ['myresponse',['myResponse',['../dc/d0f/classTCPserver.html#a8c8e2742ea5d30288d6ba547c7d90aaf',1,'TCPserver::myResponse()'],['../d4/d07/classTASK2_1_1SimFileServer.html#ab8cceff569488209f9f74bed9629880a',1,'TASK2::SimFileServer::myResponse()']]]
+];
---
+++ DOXYGENDOC/html/search/variables_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['capturedata_5f',['captureData_',['../d4/dfe/classTASK5_1_1WebCamSubject.html#ab1aee5c0bb5edf7a3daf0a264260c7ff',1,'TASK5::WebCamSubject']]]
+];
