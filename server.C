013efed5de2be2aa35a5cd52321a9fa47f12ae13
Ubuntu_16.04 <maximilian.gruendel@gmail.com>
2021-01-17 11:46:56 +0100

    Server Implementierung

--- server.C
+++ server.C
@@ -17,22 +17,78 @@
 #include <netinet/in.h> //contains constants and structures needed for internet domain addresses
 
 #include "SIMPLESOCKET.H"
-
+#include "TASK3.C"
 
 class myTCPserver : public TCPserver{
 public:
 	myTCPserver(int port, int maxDataSizeRecv) : TCPserver(port, maxDataSizeRecv){;}
+	void game();
 
 protected:
-	string        myResponse(string input);
+	string myResponse(string input);
+
+private:
+	TASK3::World ocean;
+	string hit(int x, int y);
+
 };
 
+
 string myTCPserver::myResponse(string input){
-	return string("Hallo Welt!");
+	if(input.compare(0, 8, "new game") == 0){
+		//ocean = new TASK3::World();
+		game();
+		return string("Created new Game\n");
+	}
+	else if(input.compare(0, 5, "shoot") == 0){
+		int x, y;
+		sscanf(input.c_str(), "shoot %i %i", &x, &y);
+		return hit(x, y);
+	}
+	else if(input.compare(0, 8, "Not found") == 0){
+		//ocean = new World();
+		return string("Game restarted\n");
+	}
+	else{
+	return string("Command not found\n");
+	}
 }
+
+using namespace TASK3;
+
+string myTCPserver::hit(int x, int y){
+	ShootResult tmp = ocean.shoot(x, y);
+	game();
+	switch(tmp){
+	case WATER:
+		return "WATER\n";
+		break;
+	case SHIP_HIT:
+		return "SHIP_HIT\n";
+		break;
+	case SHIP_DESTROYED:
+		return "SHIP_DESTROYED\n";
+		break;
+	case ALL_SHIPS_DESTROYED:
+		return "ALL_SHIPS_DESTROYED\n";
+			break;
+	case GAME_OVER:
+		return "GAME_OVER\n";
+		break;
+	default:
+		return "Not Found\n";
+	}
+}
+
+
+void myTCPserver::game(){
+	ocean.printBoard();
+}
+
+
 int main(){
 	srand(time(nullptr));
-	myTCPserver srv(2022,25);
+	myTCPserver srv(2025,25);
 	srv.run();
 }
 
